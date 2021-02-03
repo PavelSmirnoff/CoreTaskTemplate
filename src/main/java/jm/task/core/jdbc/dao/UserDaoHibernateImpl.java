24cd9e0a5fc9ee58c33dcd25bad5e2a85405b1c1
Pavel Smirnov <smirnov@0x55.ru>
2021-02-03 22:23:18 +0300

    Created Hibernate connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,18 +1,27 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    Session session = Util.getSessionFactory().openSession();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        session.beginTransaction();
+        // Check database version
+        String sql = "select version()";
+        String result = (String) session.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,21 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private static Connection connection = null;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/TaskJDBC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "12345678";
 
+    private static StandardServiceRegistry serviceRegistry = null;
+    private static SessionFactory sessionFactory = null;
+
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
+                Class.forName(DRIVER);
                 connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
                 connection.setAutoCommit(false);
             } catch (SQLException | ClassNotFoundException e) {
@@ -24,4 +39,29 @@ public class Util {
         }
         return connection;
     }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
 }
